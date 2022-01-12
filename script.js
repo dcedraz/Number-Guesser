39b2bd5c4bc55cc13d2114a178427848838430ab
dcedraz <dcedraz@salsify.com>
2022-01-12 23:04:15 +0000

    compareGuesses method refactoring

--- script.js
+++ script.js
@@ -8,13 +8,19 @@ const generateTarget = () => Math.floor(Math.random() * 10);
 
 const compareGuesses = (userGuess, computerGuess, targetNumber) => {
 
-    computerDiffTarget = Math.abs(targetNumber - computerGuess);
-    userDiffTarget = Math.abs(userGuess - computerGuess);
-
+    let computerDiffTarget = Math.abs(targetNumber - computerGuess);
+    let userDiffTarget = Math.abs(userGuess - computerGuess);
+ 
+    /* First I did this way
     switch (true) {
         case userDiffTarget === computerDiffTarget : return true;
         case userDiffTarget > computerDiffTarget : return true;
         case computerDiffTarget > userDiffTarget : return false;
         default: return true;
     };
+    */
+
+    // But then I realized all the above lines could be down to a single one:
+    return computerDiffTarget > userDiffTarget ? false : true;
+
 };
