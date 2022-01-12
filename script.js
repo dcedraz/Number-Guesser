9f4b1df45f69b5040434f66a25ea38fd76d24373
dcedraz <dcedraz@salsify.com>
2022-01-12 22:54:19 +0000

    Write methods generateTarget and compareGuesses

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+
+    computerDiffTarget = Math.abs(targetNumber - computerGuess);
+    userDiffTarget = Math.abs(userGuess - computerGuess);
+
+    switch (true) {
+        case userDiffTarget === computerDiffTarget : return true;
+        case userDiffTarget > computerDiffTarget : return true;
+        case computerDiffTarget > userDiffTarget : return false;
+        default: return true;
+    };
+};
